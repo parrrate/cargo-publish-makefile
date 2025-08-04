b6672b47f6fb4240253c2a590f3c74648ea71c7b
parrrate <tim@ongoteam.yaconnect.com>
2025-08-04 20:30:19 +0000

    handle running from within `cargo`

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
-use clap::Parser;
+use cargo_publish_makefile::Args;
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand)]
+enum Cmd {
+    PublishMakefile(Args),
+}
+
+#[derive(Parser)]
+struct Wrapped {
+    #[clap(subcommand)]
+    cmd: Cmd,
+}
 
 fn main() -> anyhow::Result<()> {
-    cargo_publish_makefile::Args::parse().run()
+    if std::env::current_exe()?.ends_with("cargo") {
+        let Cmd::PublishMakefile(args) = Wrapped::parse().cmd;
+        args
+    } else {
+        cargo_publish_makefile::Args::parse()
+    }
+    .run()
 }
